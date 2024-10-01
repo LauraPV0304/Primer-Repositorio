c093e3ef0f537278e9361ac2153de6d4a7af1742
Laura PV <zx23student3282@colegiovalledelmiro.es>
2024-10-01 13:43:25 +0200

    calculadora con resta

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
 numero1 = int(input("Introduce un número: "))
 numero2 = int(input("Introduce un segundo número: "))
 
-suma = numero1 + numero2
+resta = numero1 - numero2
 
 print("Resultado: \nw")
-print("Suma: ", suma)
+print("Suma: ", resta)
