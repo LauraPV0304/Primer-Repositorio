fc83ed00efdda5b5f932a987ed0fce81447cc7ad
Laura PV <zx23student3282@colegiovalledelmiro.es>
2024-10-01 13:44:07 +0200

    calculadora con multiplicacion

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
 numero1 = int(input("Introduce un número: "))
 numero2 = int(input("Introduce un segundo número: "))
 
-resta = numero1 - numero2
+multi = numero1 * numero2
 
 print("Resultado: \nw")
-print("Suma: ", resta)
+print("Suma: ", multi)
