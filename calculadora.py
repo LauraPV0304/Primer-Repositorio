f01733afc742ebca3c765143f6cac95dbb85cd57
Laura PV <zx23student3282@colegiovalledelmiro.es>
2024-10-02 09:29:43 +0200

    calculadora con division

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
 numero1 = int(input("Introduce un número: "))
 numero2 = int(input("Introduce un segundo número: "))
 
-multi = numero1 * numero2
+division = numero1 / numero2
 
 print("Resultado: \nw")
-print("Suma: ", multi)
+print("Suma: ", division)
