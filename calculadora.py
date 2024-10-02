dbd69174fcd603537a4f49aac746b12431911aab
Laura PV <zx23student3282@colegiovalledelmiro.es>
2024-10-02 09:46:00 +0200

    suma modificada

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
 numero1 = int(input("Introduce un número: "))
 numero2 = int(input("Introduce un segundo número: "))
 
-multi = numero1 * numero2
+suma = numero1 + numero2
 
-print("Resultado: \nw")
-print("Suma: ", multi)
+print("Resultado de la suma: \n")
+print(suma)
